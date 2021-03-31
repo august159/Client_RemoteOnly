0e35999bdba22aa5475080d749d71523ef6386e5
adri <ad.mallie@gmail.com>
2021-03-31 14:36:57 +0200

    oneApplicatio,

---
+++ src/components/CompanyListOffers.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import apiHandler from "../api/apiHandler";
+
+export class CompanyListOffers extends Component {
+  state = {
+    company: [],
+  };
+
+  componentDidMount() {
+    apiHandler
+      .getCompany(this.props.companyId)
+      .then((company) => {
+        this.setState({ company });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    if (this.state.company !== []) {
+      console.log("++++++++++", this.state.company);
+      return <div>{/* <p>{this.state.company.offers}</p> */}</div>;
+    }
+  }
+}
+
+export default CompanyListOffers;
--- src/pages/DashboardRecruiter.jsx
+++ src/pages/DashboardRecruiter.jsx
@@ -1,112 +1,71 @@
 import React from "react";
 import "../styles/custom.css";
-import apiHandler from "../api/apiHandler";
-import { Link} from "react-router-dom";
-import "./../styles/custom.css"
+// import apiHandler from "../api/apiHandler";
+import { Link } from "react-router-dom";
+import "./../styles/custom.css";
+import CompanyListOffers from "../components/CompanyListOffers";
+import { withUser } from "./../components/Auth/withUser";
 
-class DashboardRecruiter extends React.Component {
-  // state = {
-  //   offers: [],
-  // };
+const DashboardRecruiter = (props) => {
+  const { context } = props;
+  const companyId = context.user.companies[0];
 
-  // componentDidMount() {
-  //   apiHandler.getOffers()
-  //     .then((response) => {
-  //       this.setState({ offers: response});
-  //       console.log(`response`, response)
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  //   // console.log(this.state.offers);
-  // }
-
-  state = {
-    offers: [],
-    applications: [],
-  };
-
-  componentDidMount() {
-    apiHandler
-      .getOffers(this.state)
-      .then((offers) => {
-        this.setState({ offers });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  // componentDidMount() {
-  //   apiHandler
-  //     .getApplication(this.state)
-  //     .then((offers) => {
-  //       this.setState({ offers });
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }
-
-  render() {
-    // console.log(`this.state`, this.state);
-    // const { offers } = this.state;
-    return (
-      <section className="hero has-background-white-ter pt-0">
-        <div className="is-fullheight ">
-          <div className="hero-body ">
-            <div className="container">
-              <div className="columns box-full ">
-                <div className="column is-one-quarter">
-                  <div className="box">
-                    {/* <Link > */}
-                    {this.state.offers.map((offer) => (
-                      <div className="dashboard_offer" key={offer._id}>
-                        <Link to={`/offer/${offer._id}`} key={offer._id}></Link>
-
-                        <p>{offer.title}</p>
-                        <p>{offer.contractType}</p>
-                        <p>{offer.createdAt}</p>
-                        <hr style={{ width: "50%", textAlign: "left" }}></hr>
-                      </div>
-                    ))}
-                    {/* </Link> */}
-                  </div>
-                </div>
-                <div className="column  is-three-quarter">
-                  <div className="box">
-                    <div className="level">
-                      <div className="level-right">
-                        <p className="level-item">
-                          <strong>Toutes</strong>
-                        </p>
-                        <p className="level-item">
-                          <a>À revoir</a>
-                        </p>
-                        <p className="level-item">
-                          <a>Selectionné</a>
-                        </p>
-                      </div>
-                      <hr className="solid" />
-                    </div>
-                    <div>
-                    {this.state.applications.map((application) => (
-                      
-                        <div to={`/application/${application._id}`} key={application._id}>
-                        <p>{application.firstName}</p>
-                        <p>{application.lastName}</p>
-                        </div>
-                    ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          </div>
-        
-      </section>
-    );
-  }
-}
-
-export default DashboardRecruiter;
+  return (
+    <div>
+      <CompanyListOffers companyId={companyId} />
+    </div>
+  );
+  // <section className="hero has-background-white-ter pt-0">
+  //   <div className="is-fullheight ">
+  //     <div className="hero-body ">
+  //       <div className="container">
+  //         <div className="columns box-full ">
+  //           <div className="column is-one-quarter">
+  //             <div className="box">
+  //               <Link >
+  //               <CompanyOffers></CompanyOffers>
+  //               {this.state.offers.map((offer) => (
+  //                 <div className="dashboard_offer" key={offer._id}>
+  //                   <Link to={`/offer/${offer._id}`} key={offer._id}></Link>
+  //                   <p>{offer.title}</p>
+  //                   <p>{offer.contractType}</p>
+  //                   <p>{offer.createdAt}</p>
+  //                   <hr style={{ width: "50%", textAlign: "left" }}></hr>
+  //                 </div>
+  //               ))}
+  //               {/* </Link> */}
+  //             </div>
+  //           </div>
+  //           <div className="column  is-three-quarter">
+  //             <div className="box">
+  //               <div className="level">
+  //                 <div className="level-right">
+  //                   <p className="level-item">
+  //                     <strong>Toutes</strong>
+  //                   </p>
+  //                   <p className="level-item">
+  //                     <a>À revoir</a>
+  //                   </p>
+  //                   <p className="level-item">
+  //                     <a>Selectionné</a>
+  //                   </p>
+  //                 </div>
+  //                 <hr className="solid" />
+  //               </div>
+  //               <div>
+  //               {this.state.applications.map((application) => (
+  //                   <div to={`/application/${application._id}`} key={application._id}>
+  //                   <p>{application.firstName}</p>
+  //                   <p>{application.lastName}</p>
+  //                   </div>
+  //               ))}
+  //               </div>
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //     </div>
+  // </section>
+};
+export default withUser(DashboardRecruiter);
