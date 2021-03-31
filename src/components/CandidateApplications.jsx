cb52d0d458c01bf9f1e44496e00a57925da717b0
Yassine <yassineabouzaid@hotmail.com>
2021-03-31 20:16:52 +0200

    Front changed into Dashboard Candidate

--- src/components/CandidateApplications.jsx
+++ src/components/CandidateApplications.jsx
@@ -38,7 +38,7 @@ export class CandidateApplications extends Component {
       <div>
         {this.state.appInfo && (
           <>
-            <div className="box offerDetails">
+            {/* <div className="box offerDetails">
               <h1>{offer.title}</h1>
               <p>{offer.fieldWork}</p>
               <p>{offer.contractType}</p>
@@ -50,26 +50,26 @@ export class CandidateApplications extends Component {
               <p>{offer.profileDescription}</p>
               <h4>Processus de recrutement</h4>
               <p>{offer.recruitmentProcess}</p>
-            </div>
-            <div className="box applicationDetails">
+            </div> */}
+            <div className=" applicationDetails">
               <h1 className="title is-4">Ma candidature</h1>
-              <h2 className="title is-4">
+              <h2 className="title is-5">
                 {firstName} {lastName}
               </h2>
-              <h4 className="title is-4">Portfolio:</h4>
+              <h4 className="title is-5">Portfolio:</h4>
               <ul>
                 {linkedIn && <li>LinkedIn: {linkedIn}</li>}
                 {gitHub && <li>GitHub: {gitHub}</li>}
                 {otherWebsite && <li>Other website: {otherWebsite}</li>}
               </ul>
-              <h4 className="title is-4">C.V.: </h4>
+              <h4 className="title is-5">C.V.: </h4>
               <embed
                 src={resume}
                 width={800}
                 height={500}
                 type="application/pdf"
               />
-              <h4 className="title is-4">Informations supplémentaires:</h4>
+              <h4 className="title is-5">Informations supplémentaires:</h4>
               <p>{additionalInfo}</p>
             </div>
             {/* <button className="button is-info">Modifier ma candidature</button> */}
