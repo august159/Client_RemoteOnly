29b965a4d7ad0c7107e83cca42473c926f4db63b
adri <ad.mallie@gmail.com>
2021-03-31 10:27:24 +0200

    application form style

--- src/components/Forms/ApplicationForm.jsx
+++ src/components/Forms/ApplicationForm.jsx
@@ -102,80 +102,150 @@ export class ApplicationForm extends Component {
       return <div>...Loading...</div>;
     }
     return (
-      <form onSubmit={this.handleSubmit} encType="multipart/form-data">
-        <label htmlFor="firstName">Prénom*</label>
-        <input
-          id="firstName"
-          onChange={this.handleChange}
-          value={this.state.firstName}
-          name="firstName"
-          type="text"
-        />
-        <label htmlFor="lastName">NOM*</label>
-        <input
-          value={this.state.lastName}
-          onChange={this.handleChange}
-          name="lastName"
-          id="lastName"
-          type="text"
-        />
-        <label htmlFor="email">Email*</label>
-        <input
-          onChange={this.handleChange}
-          value={this.state.email}
-          name="email"
-          id="email"
-          type="text"
-        />
-        <label htmlFor="phone">Téléphone*</label>
-        <input
-          value={this.state.phone}
-          onChange={this.handleChange}
-          name="phone"
-          id="phone"
-          type="text"
-        />
-        <label htmlFor="linkedIn">Lnkedin</label>
-        <input
-          onChange={this.handleChange}
-          value={this.state.linkedIn}
-          name="linkedIn"
-          id="linkedIn"
-          type="text"
-        />
-        <label htmlFor="gitHub">Github</label>
-        <input
-          onChange={this.handleChange}
-          value={this.state.gitHub}
-          name="gitHub"
-          id="gitHub"
-          type="text"
-        />
-        <label htmlFor="otherWebsite">Autre website</label>
-        <input
-          onChange={this.handleChange}
-          value={this.state.otherWebsite}
-          name="otherWebsite"
-          id="otherWebsite"
-          type="text"
-        />
-        <label htmlFor="resume">CV</label>
-        <input
-          onChange={this.handleUpload}
-          name="resume"
-          id="resume"
-          type="file"
-          id="resume"
-        />
-        <label htmlFor="additionalInfo">Ajouter un message</label>
-        <textarea
-          onChange={this.handleChange}
-          value={this.state.additionalInfo}
-          name="additionalInfo"
-          id="additionalInfo"
-        />
-        <button>Postuler</button>
-      </form>
+      <div className="box">
+        <h2>Postulez pour ce job</h2>
+        <form onSubmit={this.handleSubmit} encType="multipart/form-data">
+          <label className="label" htmlFor="firstName">
+            Prénom*
+          </label>
+          <input
+            id="firstName"
+            onChange={this.handleChange}
+            value={this.state.firstName}
+            name="firstName"
+            type="text"
+            className="input"
+          />
+          <br />
+          <br />
+          <label htmlFor="lastName" className="label">
+            NOM*
+          </label>
+          <input
+            value={this.state.lastName}
+            onChange={this.handleChange}
+            name="lastName"
+            id="lastName"
+            type="text"
+            className="input"
+          />
+          <br />
+          <br />
+          <label htmlFor="email" className="label">
+            Email*
+          </label>
+          <input
+            onChange={this.handleChange}
+            value={this.state.email}
+            name="email"
+            id="email"
+            type="text"
+            className="input"
+          />
+          <br />
+          <br />
+          <label htmlFor="phone" className="label">
+            Téléphone*
+          </label>
+          <input
+            value={this.state.phone}
+            onChange={this.handleChange}
+            name="phone"
+            id="phone"
+            type="text"
+            className="input"
+          />
+          <br />
+          <br />
+          <label htmlFor="linkedIn" className="label">
+            Lnkedin
+          </label>
+          <input
+            onChange={this.handleChange}
+            value={this.state.linkedIn}
+            name="linkedIn"
+            id="linkedIn"
+            type="text"
+            className="input"
+          />
+          <br />
+          <br />
+
+          <label htmlFor="gitHub" className="label">
+            Github
+          </label>
+          <input
+            onChange={this.handleChange}
+            value={this.state.gitHub}
+            name="gitHub"
+            id="gitHub"
+            type="text"
+            className="input"
+          />
+          <br />
+          <br />
+
+          <label htmlFor="otherWebsite" className="label">
+            Autre website
+          </label>
+          <input
+            onChange={this.handleChange}
+            value={this.state.otherWebsite}
+            name="otherWebsite"
+            id="otherWebsite"
+            type="text"
+            className="input"
+          />
+          <br />
+          <br />
+
+          <div className="file">
+            <label className="file-label">
+              <input
+                className="file-input"
+                onChange={this.handleUpload}
+                name="resume"
+                id="resume"
+                type="file"
+                id="resume"
+              />
+              <span className="file-cta">
+                <span className="file-icon">
+                  <i className="fas fa-upload"></i>
+                </span>
+                <span className="file-label">... Ajoutez votre CV</span>
+              </span>
+            </label>
+          </div>
+          <br />
+
+          {/* <div className="file">
+          <label htmlFor="resume" className="file-label">
+            CV
+          </label>
+          <input
+            onChange={this.handleUpload}
+            name="resume"
+            id="resume"
+            type="file"
+            id="resume"
+            className="file-input"
+          />
+        </div> */}
+          <label htmlFor="additionalInfo" className="label">
+            Ajouter un message
+          </label>
+          <textarea
+            onChange={this.handleChange}
+            value={this.state.additionalInfo}
+            name="additionalInfo"
+            id="additionalInfo"
+            className="textarea"
+          />
+          <br />
+          <button className="button is-primary">Postuler</button>
+        </form>
+      </div>
     );
   }
 }
